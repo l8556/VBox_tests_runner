a2efb98805de9d116f4a5e9b94fc4f3e97ada87c
l02 <l8556@outlook.com>
2023-06-14 16:36:11 +0300

    worked

--- tasks.py
+++ tasks.py
@@ -3,15 +3,12 @@ import os
 from os.path import join
 
 from invoke import task
+from rich.prompt import Prompt
 
-from frameworks.VBox import VirtualMachine
 from frameworks.host_control import FileUtils
 from tests.desktop_tests import DesktopTests
 
 @task
 def desktop_test(c, version=None):
-    config = FileUtils.read_json(join(os.getcwd(), 'config.json'))
-    version = version if version else config['version']
+    version = version if version else Prompt.ask('[red]Please enter version')
     DesktopTests(version=version).run()
-    # vm = VirtualMachine('Kubuntu')
-    # print(vm.get_logged_user())
