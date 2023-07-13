6e376b1bc3904ef4b66ae05fbaaebf659c90ae01
l02 <l8556@outlook.com>
2023-07-13 17:45:23 +0300

    .

--- tasks.py
+++ tasks.py
@@ -49,7 +49,8 @@ def run_vm(c, name: str = '', headless=False):
     vm = VirtualMachine(Vbox().check_vm_names(name))
     vm.run(headless=headless)
     vm.wait_net_up(status_bar=True)
-    return print(vm.get_ip()), print(vm.wait_logged_user())
+    vm.wait_logged_user(status_bar=True)
+    return print(vm.get_ip()), print(vm.get_logged_user())
 
 @task
 def stop_vm(c, name: str = '', all: bool = False):
