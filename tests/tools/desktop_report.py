19c70b73357de1cfb5597c182437707c4cdf8266
l02 <l8556@outlook.com>
2023-07-19 20:06:21 +0300

    test

--- tests/tools/desktop_report.py
+++ tests/tools/desktop_report.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from os.path import join, isfile
+from os.path import join, isfile, basename, dirname
+import pandas as pd
 
 from frameworks.host_control import FileUtils
 from frameworks.report import Report
@@ -13,16 +14,28 @@ class DesktopReport:
         FileUtils.create_dir(self.dir, silence=True)
 
     def write(self, vm_name: str, exit_code: str):
-        if not isfile(self.path):
-            self._writer(self.path, 'w', ['Os', 'Vm_name', 'Version', 'Package_name', 'Exit_code'])
+        self._write_titles() if not isfile(self.path) else ...
         self._writer(self.path, 'a', ["", vm_name, self.version, "", exit_code])
+
+    def get_full(self, title_report: str) -> str:
+        paths = FileUtils.get_paths(self.dir, name_include=f"{self.version}", extension='csv')
+        full_report = join(self.dir, f"{self.version}_{title_report if title_report else ''}_full_report.csv")
+        FileUtils.delete(full_report, silence=True) if isfile(full_report) else ...
+        self._handle_results(paths)
+        Report().merge(paths, full_report)
+        return full_report
+
+    @staticmethod
+    def _handle_results(paths: list):
+        for report_path in paths if paths else []:
+            df = pd.read_csv(report_path)
+            df['Vm_name'] = df.apply(lambda row: f"{row['Os']} {row['Version']}", axis=1)
+            df.at[0, 'Vm_name'] = basename(dirname(report_path))
+            df.to_csv(report_path, index=False, mode='a')
 
     @staticmethod
     def _writer(file_path: str, mode: str, message: list, delimiter='\t', encoding='utf-8'):
         Report.write(file_path, mode, message, delimiter, encoding)
 
-    def get_full(self, title_report: str) -> str:
-        full_report = join(self.dir, f"{self.version}_{title_report if title_report else ''}_full_report.csv")
-        FileUtils.delete(full_report, silence=True) if isfile(full_report) else ...
-        Report().merge(FileUtils.get_paths(self.dir, name_include=f"{self.version}", extension='csv'), full_report)
-        return full_report
+    def _write_titles(self):
+        self._writer(self.path, 'w', ['Os', 'Vm_name', 'Version', 'Package_name', 'Exit_code'])
